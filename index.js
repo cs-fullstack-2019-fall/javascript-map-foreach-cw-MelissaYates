569972c095b7c8bf3e6a2d1b15d003e3e4602a44
autumn-ragland <raglanda998@gmail.com>
2019-08-15 14:08:13 -0500

    Graded 08-15-2019 14:05:45

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 // Problem 1
 // Ask the user to enter a word. Keep asking them to enter a word until they
 // enter the letter 'q'.
-// var userInput = prompt("Please enter a word. Enter 'q' to quit");
-// while(userInput!=='q'){
-//     userInput = prompt("Please enter a word. Enter 'q' to quit");
-//    }
+var userInput = prompt("Please enter a word. Enter 'q' to quit");
+while(userInput!=='q'){
+    userInput = prompt("Please enter a word. Enter 'q' to quit");
+   }
 //
 //     Problem 2
 // Make any string array all lower case strings.
 // If you don't remember how to use that Google it.
 //
 var words = ["FULL", "IS", "THE", "HE"];
+// map iterates. You do not need to nest it in a for loop
 for (var i = 0; i < words.length; i++) {
 
     var arrayLower = words.map(function (eachElement) {
